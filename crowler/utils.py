5ece3341d1cba91c3b6f3226f10bd327176d5b31
Renan <alvesrenanpro@gmail.com>
2024-01-11 00:42:03 +0000

    chore: unusued funcs

--- crowler/utils.py
+++ crowler/utils.py
@@ -1,34 +1,5 @@
-from pathlib import Path
-import os
-import logging
+
 import re
-
-FILES = "files"
-JSON = "json"
-
-def path(pathtype, *args):
-    TAG = "files"
-    try:
-        return Path(os.getcwd(), "crowler", pathtype, *args)
-    except FileNotFoundError:
-        logging.error("{}: The file path does not exists" % TAG)
-
-    return Path(os.getcwd(), "crowler", "files")
-
-def jsonpath(*args):
-    return path(JSON, *args)
-
-
-def listpath(*args):
-    return os.listdir(path(FILES, *args))
-
-
-def filepath(*args):
-    return f"file://{path(FILES, *args)}"
-
-
-def filename(file):
-    return file.split('.')[0]
 
 def sanitize(text):
     return re.compile('<.*?>').sub('', text).strip()
