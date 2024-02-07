dfeb94e40adfac488a766a7a23f7c3c1d1440893
Renan <alvesrenanpro@gmail.com>
2024-02-07 14:45:58 -0300

    chore: changed logic to read links

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use std::fs::File;
-use std::io::Read;
+use std::fs::read_to_string;
 use std::error::Error;
 
-const URLS_DIR: &str = "urls.txt";
-
+const URLS_DIR: &str = "./urls.txt";
+const OUT_DIR: &str = "./out";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let html = reqwest::blocking::get("https://www2.camara.leg.br/legin/fed/lei/2021/lei-14133-1-abril-2021-791222-publicacaooriginal-162591-pl.html")?.text();
-
-    println!("{:?}", html);
-
-    Ok(())
-}
-
-fn get_urls() -> Result<(), Box<dyn Error>> {
-    let mut reader = File::open(URLS_DIR)?;
-    let mut urls = vec![];
-
-    reader.read_to_end(&mut urls)?;
+    for url in read_to_string(URLS_DIR).unwrap().lines() {
+        // let html = reqwest::blocking::get(url)?.text();
+        println!("{:?}", url);
+    }
 
     Ok(())
 }
